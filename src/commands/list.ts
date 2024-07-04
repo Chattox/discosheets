fab00cc9ca2f318d3fe5e9817c4d73e524be310e
Conrad Lineker-Jones <33847899+Chattox@users.noreply.github.com>
2024-07-04 15:46:18 +0100

    feat(command): list command to show list of doctors added to sheet (#5)

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,9 +1,35 @@
-import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import {
+  APIEmbedField,
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} from "discord.js";
 
 export const data = new SlashCommandBuilder()
   .setName("list")
-  .setDescription("Shows all rows added to the sheet");
+  .setDescription("Shows a list of doctors currently added to the sheet");
 
-export const run = async (interaction: CommandInteraction) => {
-  console.log(interaction);
+export const run = async (interaction: ChatInputCommandInteraction) => {
+  const sheet = interaction.client.googleSheet?.sheetsByIndex[0];
+  const rows = await sheet?.getRows<RowData>({ limit: 25 });
+
+  const embedFields = rows?.map((row) => {
+    const name = row.get("name");
+    const practice = row.get("practice");
+    const location = row.get("location");
+    const comments = row.get("comments");
+
+    return {
+      name,
+      value: `Practice: ${practice}
+      Location: ${location}
+      Comments: ${comments}`,
+    };
+  });
+
+  const replyEmbed = new EmbedBuilder()
+    .setColor("Green")
+    .setTitle("List of Drs")
+    .addFields(embedFields as APIEmbedField[]);
+  await interaction.reply({ embeds: [replyEmbed], ephemeral: true });
 };
