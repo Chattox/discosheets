0599814eb315118f0fcf8ebd95744be06a1d59f0
Chattox <conradlineker@gmail.com>
2024-07-04 17:13:23 +0100

    feat(command): addprovider command for adding providers to spreadsheet

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -7,7 +7,7 @@ import {
 
 export const data = new SlashCommandBuilder()
   .setName("list")
-  .setDescription("Shows a list of doctors currently added to the sheet");
+  .setDescription("Shows a list of providers currently added to the sheet");
 
 export const run = async (interaction: ChatInputCommandInteraction) => {
   const sheet = interaction.client.googleSheet?.sheetsByIndex[0];
@@ -29,7 +29,7 @@ export const run = async (interaction: ChatInputCommandInteraction) => {
 
   const replyEmbed = new EmbedBuilder()
     .setColor("Green")
-    .setTitle("List of Drs")
+    .setTitle("List of Providers")
     .addFields(embedFields as APIEmbedField[]);
   await interaction.reply({ embeds: [replyEmbed], ephemeral: true });
 };
